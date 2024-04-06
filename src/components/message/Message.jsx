f209af0f4f1917a1d7b13b156c1f9f8fb9828db5
iirrrrffan <irfanali376246@gmail.com>
2024-04-06 19:48:40 +0530

    chat socket

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./message.css"
-const Message = ({own}) => {
+import {formate,format} from "timeago.js"
+
+const Message = ({Message,own}) => {
   return (
     <div className={own ? "message own" : "message"}>
     <div className="messageTop">
@@ -9,9 +11,9 @@ const Message = ({own}) => {
         src="https://i.pinimg.com/564x/6a/fc/5c/6afc5c43a5050054d7482202e3b75239.jpg"
         alt=""
       />
-      <p className="messageText">Khan made his film debut with a small role in Salaam Bombay!, </p>
+      <p className="messageText">{Message.text} </p>
     </div>
-    <div className="messageBottom">1 hour ago</div>
+    <div className="messageBottom">{format(Message.createdAt)}</div> 
   </div>
 
   )
--- src/pages/messanger/Messanger.jsx
+++ src/pages/messanger/Messanger.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import Topbar from '../../components/topbar/Topbar'
 import Conversation from '../../components/conversation/Conversation'
 import Message from '../../components/message/Message'
@@ -7,18 +7,35 @@ import axios from 'axios'
 import Sidebar from '../../components/sidebar/Sidebar'
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../context/AuthContext'
+import {io} from "socket.io-client"
 
 const Messanger = () => {
   const navigation = useNavigate()
   const [users,setUsers]=useState(null);
   const [conversation,setConversation]=useState([])
+  const [currentChat,setCurrentChat]=useState(null)
+  const [socket,setSocket]=useState(null)
+  const [messages,setMessages]=useState([])
+  const [newMessage,setNewMessage]=useState("")
+  const scrollRef = useRef();
+
 const {user} = useContext(AuthContext)
+
+
+useEffect(()=>{
+   setSocket(io("ws:http://localhost:8900"))
+},[])
+
+useEffect(()=>{
+  
+})
 
 useEffect(()=>{
   const getConversaton = async ()=>{
     try{
      const res = await axios.get(`http://localhost:3006/api/conversation/${user._id}`)
      setConversation(res.data)
+     console.log(res);
     }catch(error){
    console.log(error);
     }
@@ -39,7 +56,42 @@ useEffect(()=>{
  }
 },[])
 
+useEffect(() => {
+  const getMessages = async () => {
+    try {
+      const res = await axios.get(`http://localhost:3006/api/messages/${currentChat._id}`);
+      setMessages(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  if (currentChat) {
+    getMessages();
+  }
+}, [currentChat, user._id]);
+ 
+ 
+ 
 
+ const handleSubmit = async(e)=>{
+  e.preventDefault();
+  const message = {
+    sender : user._id,
+    text : newMessage,
+    conversationId : currentChat._id,
+  };
+  try{
+ const res = await axios.post("http://localhost:3006/api/messages",message)
+ setMessages([...messages,res.data]);
+setNewMessage("");
+  }catch(error){
+    console.log(error);
+  }
+ }
+
+ useEffect(()=>{
+ scrollRef.current?.scrollIntoView({behavior:"smooth"})
+ },[messages])
 
 
   return (
@@ -54,43 +106,40 @@ useEffect(()=>{
       <div className="chatMenu" style={{width:"30%", flex:"none"}}>
         <div className="chatMenuWrapper">
           <input placeholder="Search for friends" className="chatMenuInput" />
-            <div>
-              
+         
            {conversation.map(c=>(
+            <div onClick={()=>setCurrentChat(c)}>
             <Conversation conversation={c} currentUser={user} key={c._id}/>
-           ))}
-
             </div>
+           ))}
         </div>
       </div>
       <div className="chatBox" style={{ width:"940px"}}>
         <div className="chatBoxWrapper">
-         
+         {currentChat ? (
+        
             <>
               <div className="chatBoxTop">
-               
-                  <div >
-                    <Message  />
-                    <Message own={true} />
-                    <Message  />
-                    <Message own={true} />
-                    <Message  />
+               {messages.map((m,index)=>(
+                <div ref={scrollRef}>
+               <Message Message={m} own={m.sender === user._id} key={index}/>
+               </div>
+               ))}
+                 
 
-                  </div>
-              
               </div>
               <div className="chatBoxBottom">
                 <textarea
                   className="chatMessageInput"
                   placeholder="write something..."
+                  onChange={(e)=>setNewMessage(e.target.value)}
+                  value={newMessage}
                 ></textarea>
-                <button className="chatSubmitButton" >
+                <button className="chatSubmitButton" onClick={handleSubmit} >
                   Send
                 </button>
               </div>
-            </>
-            <span className="noConversationText">
-            </span>
+            </> ):( <span className='noConversationText '>Open a conversation to start a chat</span>)}
         </div>
       </div>
     </div>
