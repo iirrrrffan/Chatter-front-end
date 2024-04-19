223e341ff237e42a871545ad6f66fd693d36f725
iirrrrffan <irfanali376246@gmail.com>
2024-04-19 14:30:33 +0530

    success following and unfollowing

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,40 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import "./home.css"
-import Topbar from '../../components/topbar/Topbar'
-import Sidebar from '../../components/sidebar/Sidebar'
-import Rightbar from '../../components/rightbar/Rightbar'
-import { useNavigate } from 'react-router-dom'
-
-
+import React, { useEffect, useState } from 'react';
+import "./home.css";
+import Topbar from '../../components/topbar/Topbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Rightbar from '../../components/rightbar/Rightbar';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigation = useNavigate();
+  const [user, setUser] = useState(null);
 
-  const navigation = useNavigate()
-  const [user,setUser]=useState(null);
+  useEffect(() => {
+    const storedUser = window.localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
 
-useEffect(()=>{
- const storedUser = window.localStorage.getItem("user")
- if(storedUser){
-  setUser(JSON.parse(storedUser));
- }
- 
- if(storedUser){
-  navigation("/home")
- }else{
-  navigation("/")
- }
-},[])
+    if (storedUser) {
+      navigation("/home");
+    } else {
+      navigation("/");
+    }
+  }, []);
 
   return (
-    <div>
-    <Topbar/>
-    <div className='homeContainer'>
-    <Sidebar/>
-    
-    <Rightbar/>
+    <div className="homePage1">
+      <Topbar />
+      <div className='homeContainer1'>
+        <Sidebar />
+        <Rightbar />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/pages/userProfile/UserProfile.jsx
+++ src/pages/userProfile/UserProfile.jsx
@@ -6,25 +6,63 @@ import axios from 'axios';
 
 const UserProfile = () => {
   const [state,setState]= useState()
+  const [userData, setUserData] = useState(null);
   const { _id } = useParams();
   const [isFollowing, setIsFollowing] = useState(false);
+  const userId = _id;
 
   const handleFollowToggle = () => {
     setIsFollowing(prevState => !prevState);
   };
+
+  useEffect(() => {
+    const userData = window.localStorage.getItem("user");
+    if (userData) {
+      setUserData(JSON.parse(userData));
+    }
+  }, []);
+
+
   const fecthdata = async()=>{
+    const logUserId = userData ? userData._id : null;
+    console.log(logUserId,'sdfghjkl')
     try {
-      const res = await axios.get(`http://localhost:3006/api/users/userbyId/${_id}`)
-    
+      const res = await axios.get(`http://localhost:3006/api/users/userbyId/${userId}`)
+      console.log(res,'dsfghjk')
+      setIsFollowing(res.data.user.followers.includes(logUserId));
       setState(res.data.user)
+   
     } catch (error) {
       console.log(error);
     }
   }
-  useEffect(()=>{
+  useEffect(()=>{          
     fecthdata()
-  })
-console.log(state);
+  }, [userId, userData])
+  
+  const handleFollow = async () => {
+    try {
+      const logUserId = userData ? userData._id : null;
+      
+      if (logUserId) {
+        if (isFollowing) {
+          await axios.put(`http://localhost:3006/api/users/${logUserId}/unfollow`, {
+            userId: userId, // Send userId in the request body
+          });
+          setIsFollowing(false); // Update state
+        } else {
+          await axios.put(`http://localhost:3006/api/users/${logUserId}/follow`, {
+            userId: userId, // Send userId in the request body
+          });
+          setIsFollowing(true); // Update state
+        }
+      }
+    } catch (error) {
+      console.error("Error occurred while following/unfollowing user:", error);
+    }
+  };
+  
+  
   return (
     <>
       {/* <Topbar /> */}
@@ -46,21 +84,21 @@ console.log(state);
             </div>
             <div className="userprofileInfo">
               <h2 className="userprofileInfoName">{state?.username}</h2>
-              <h1 style={{ fontSize: 20 }}>following 3</h1>
-              <h1 style={{ fontSize: 20 }}>followers 5</h1>
+              <h1 style={{ fontSize: 20 }}>following {state?.followings.length}</h1>
+              <h1 style={{ fontSize: 20 }}>followers {state?.followers.length}</h1>
               <div className="email">
                 <h1 style={{ fontFamily: 'Arial', fontSize: 30 }}>Email</h1>
                 <h1>{state?.email}</h1>
               </div>
               {isFollowing ? (
                 <>
-                  <button className="unfollowButton" onClick={handleFollowToggle}>
+                  <button className="unfollowButton" onClick={handleFollow}>
                     Unfollow
                   </button>
                   <button className="messageButton">Message</button>
                 </>
               ) : (
-                <button className="followButton" onClick={handleFollowToggle}>
+                <button className="followButton" onClick={handleFollow}>
                   Follow
                 </button>
               )}
